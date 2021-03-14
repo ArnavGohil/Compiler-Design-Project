1c14e78b265fac89ec4f6e809fa5615a09d19d24
Arnav <arnav.gohil04@gmail.com>
2021-03-14 10:37:27 +0530

    :rocket:

--- app/src/main/java/com/example/myapplication/CameraActivity.java
+++ app/src/main/java/com/example/myapplication/CameraActivity.java
@@ -1,43 +1,51 @@
 package com.example.myapplication;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.ActivityOptions;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.material.chip.ChipGroup;
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 import com.google.android.material.progressindicator.CircularProgressIndicator;
 
 public class CameraActivity extends AppCompatActivity {
 
-    CircularProgressIndicator cir ;
+    CircularProgressIndicator cir;
     ChipGroup cg;
-    TextView gra ;
-    String str =" E = aAb | c \n" +
-                " F = aa \n" +
-                " T = b " ;
+    TextView gra;
+    ExtendedFloatingActionButton fab;
+    String str = " E = aAb | c \n" +
+            " F = aa \n" +
+            " T = b ";
+    int but_id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(intent,0);
+        startActivityForResult(intent, 0);
         setContentView(R.layout.activity_camera);
 
         cg = findViewById(R.id.chip_group);
         cir = findViewById(R.id.cir);
         gra = findViewById(R.id.grammar);
+        fab = findViewById(R.id.fab);
 
+        cg.setOnCheckedChangeListener((group, checkedId) -> {
+            but_id = checkedId;
+            fab.setVisibility(View.VISIBLE);
+        });
 
+        fab.setOnClickListener(view -> {
+            Toast.makeText(this, "" + but_id, Toast.LENGTH_SHORT).show();
+        });
 
 
     }
@@ -51,7 +59,7 @@ public class CameraActivity extends AppCompatActivity {
         new Handler().postDelayed(() -> {
             cir.setVisibility(View.INVISIBLE);
             gra.setText(str);
-        },2000);
+        }, 2000);
 
     }
 }
