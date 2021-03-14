7e20f3f7af54da7cf2f4aecd5221b5b4202be274
Arnav <arnav.gohil04@gmail.com>
2021-03-14 11:51:44 +0530

    :rocket:

--- app/src/main/java/com/example/myapplication/CameraActivity.java
+++ app/src/main/java/com/example/myapplication/CameraActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Base64;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -14,6 +15,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.chip.ChipGroup;
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 import com.google.android.material.progressindicator.CircularProgressIndicator;
+
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class CameraActivity extends AppCompatActivity {
 
@@ -55,6 +67,33 @@ public class CameraActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         Bundle extras = data.getExtras();
         Bitmap imageBitmap = (Bitmap) extras.get("data");
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        imageBitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+        byte[] byteArray = byteArrayOutputStream.toByteArray();
+
+        String encoded = Base64.encodeToString(byteArray, Base64.DEFAULT);
+
+        try {
+            OkHttpClient client = new OkHttpClient().newBuilder()
+                    .build();
+            MediaType mediaType = MediaType.parse("application/json");
+            RequestBody body = RequestBody.create(mediaType, "{\"requests\":[{\"image\":{\"content\":\"" + encoded + "\"},\"features\":[{\"type\":\"DOCUMENT_TEXT_DETECTION\"}]}]}");
+            Request request = new Request.Builder()
+                    .url("https://vision.googleapis.com/v1/images:annotate?key=" + R.string.key)
+                    .method("POST", body)
+                    .addHeader("Content-Type", "application/json")
+                    .build();
+            Response response = client.newCall(request).execute();
+
+            JSONObject obj = new JSONObject(response.body().string());
+
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
 
         new Handler().postDelayed(() -> {
             cir.setVisibility(View.INVISIBLE);
