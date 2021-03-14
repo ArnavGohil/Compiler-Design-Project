dd31a7f43229cd5b128d2c7a3a792dd2e9b517f1
Arnav <arnav.gohil04@gmail.com>
2021-03-14 19:08:18 +0530

    :rocket:

--- app/src/main/java/com/example/myapplication/ResultActivity.java
+++ app/src/main/java/com/example/myapplication/ResultActivity.java
@@ -3,8 +3,12 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
+
+    int parser ;
+    String grammar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,9 +16,38 @@ public class ResultActivity extends AppCompatActivity {
         setContentView(R.layout.activity_result);
 
         Bundle bundle = getIntent().getExtras();
-        int parser = bundle.getInt("PARSER");
-        String grammar = bundle.getString("GRAMMAR");
+        parser = bundle.getInt("PARSER");
+        grammar = bundle.getString("GRAMMAR");
 
+        TextView tt = findViewById(R.id.tt);
+        String s = "";
+        switch (parser)
+        {
+            case 1 : s = "LL(1)" ;
+
+
+                break;
+            case 2 : s = "LR(0)" ;
+
+
+                break;
+            case 3 : s = "SLR(1)" ;
+
+
+                break;
+            case 4 : s = "LALR(1)" ;
+
+
+                break;
+            case 5 : s = "CLR(1)" ;
+
+
+                break;
+        }
+        tt.setText(s);
 
     }
+
+
+
 }
