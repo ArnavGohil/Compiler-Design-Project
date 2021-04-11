478d3aca9b144d7b74816e913b7487f8651d0455
Arnav <arnav.gohil04@gmail.com>
2021-04-12 00:56:29 +0530

    :rocket:

--- app/src/main/java/com/example/myapplication/ResultActivity.java
+++ app/src/main/java/com/example/myapplication/ResultActivity.java
@@ -1,19 +1,24 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.myapplication.Parsers.lr0.LR0Parser;
+import com.example.myapplication.Parsers.lr1.LR1Parser;
 import com.example.myapplication.Parsers.util.Grammar;
 import com.google.android.material.textfield.TextInputEditText;
 
 public class ResultActivity extends AppCompatActivity {
 
-    int parser ;
+    int parser = 1;
     String s = "";
     String grammar;
+    String aug = "", first = "", follow = "", canon = "", goat = "", act = "";
+    LR0Parser lr0Parser;
+    LR1Parser lr1Parser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,43 +26,152 @@ public class ResultActivity extends AppCompatActivity {
         setContentView(R.layout.activity_result);
 
         Bundle bundle = getIntent().getExtras();
-        parser = bundle.getInt("PARSER");
-        grammar = bundle.getString("GRAMMAR");
+        //parser = bundle.getInt("PARSER");
+        //grammar = bundle.getString("GRAMMAR");
+//        grammar = grammar.replaceAll("=","->");
 
         TextView tt = findViewById(R.id.tt);
+        TextView area = findViewById(R.id.textArea);
 
-        TextInputEditText txt = findViewById(R.id.inpSTR);
+        TextInputEditText txt = findViewById(R.id.inp);
         // TODO Here
 
         //TODO - Works well
-        String grammarText =  "S -> A\n" +
+        String grammarText = "S -> A\n" +
                 "S -> B\n" +
                 "A -> b\n" +
-                "B -> a" ;
+                "B -> a";
         Grammar grammar = new Grammar(grammarText);
-        LR0Parser lr0Parser = new LR0Parser(grammar);
-        //area.setText(lr0Parser.getGrammar()+"");
 
-        switch (parser)
-        {
-            case 1 : s = "LR(0)" ;
+        area.setText("\"\"\"\"\"\"\"");
+
+        switch (parser) {
+            case 1:
+                s = "LR(0)";
                 tt.setText(s);
+                lr0Parser = new LR0Parser(grammar);
+
+                if (!lr0Parser.parserLR0())
+                    cannotParse();
+
+                aug = lr0Parser.getGrammar() + "";
+
+                for (String s : lr0Parser.getGrammar().getFirstSets().keySet()) {
+                    first += s + " : " + lr0Parser.getGrammar().getFirstSets().get(s) + "\n";
+                }
+
+                for (String s : lr0Parser.getGrammar().getFallowSets().keySet()) {
+                    follow += s + " : " + lr0Parser.getGrammar().getFallowSets().get(s) + "\n";
+                }
+
+                canon = lr0Parser.canonicalCollectionStr();
+
+                goat = lr0Parser.goToTableStr();
+
+                act = lr0Parser.actionTableStr();
+
                 break;
-            case 2 : s = "SLR(1)" ;
+
+
+            case 2:
+                s = "SLR(1)";
                 tt.setText(s);
-                //SLRparser();
+                lr0Parser = new LR0Parser(grammar);
+
+                if (!lr0Parser.parserSLR1())
+                    cannotParse();
+
+                aug = lr0Parser.getGrammar() + "";
+
+                for (String s : lr0Parser.getGrammar().getFirstSets().keySet()) {
+                    first += s + " : " + lr0Parser.getGrammar().getFirstSets().get(s) + "\n";
+                }
+
+                for (String s : lr0Parser.getGrammar().getFallowSets().keySet()) {
+                    follow += s + " : " + lr0Parser.getGrammar().getFallowSets().get(s) + "\n";
+                }
+
+                canon = lr0Parser.canonicalCollectionStr();
+
+                goat = lr0Parser.goToTableStr();
+
+                act = lr0Parser.actionTableStr();
+
                 break;
-            case 3 : s = "LALR(1)" ;
+
+
+            case 3:
+                s = "LALR(1)";
                 tt.setText(s);
-                //LALRparser();
+                lr1Parser = new LR1Parser(grammar);
+
+                if (!lr1Parser.parseLALR1())
+                    cannotParse();
+
+                aug = lr1Parser.getGrammar() + "";
+
+                for (String s : lr1Parser.getGrammar().getFirstSets().keySet()) {
+                    first += s + " : " + lr1Parser.getGrammar().getFirstSets().get(s) + "\n";
+                }
+
+                for (String s : lr1Parser.getGrammar().getFallowSets().keySet()) {
+                    follow += s + " : " + lr1Parser.getGrammar().getFallowSets().get(s) + "\n";
+                }
+
+                canon = lr1Parser.canonicalCollectionStr();
+
+                goat = lr1Parser.goToTableStr();
+
+                act = lr1Parser.actionTableStr();
+
                 break;
-            case 4 : s = "CLR(1)" ;
+
+
+            case 4:
+                s = "CLR(1)";
                 tt.setText(s);
-                //CLRparser();
+                lr1Parser = new LR1Parser(grammar);
+
+                if (!lr1Parser.parseLALR1())
+                    cannotParse();
+
+                aug = lr1Parser.getGrammar() + "";
+
+                for (String s : lr1Parser.getGrammar().getFirstSets().keySet()) {
+                    first += s + " : " + lr1Parser.getGrammar().getFirstSets().get(s) + "\n";
+                }
+
+                for (String s : lr1Parser.getGrammar().getFallowSets().keySet()) {
+                    follow += s + " : " + lr1Parser.getGrammar().getFallowSets().get(s) + "\n";
+                }
+
+                canon = lr1Parser.canonicalCollectionStr();
+
+                goat = lr1Parser.goToTableStr();
+
+                act = lr1Parser.actionTableStr();
+
                 break;
         }
+
+
+
+
+
+
+
+
+
     }
 
+    void cannotParse() {
+        AlertDialog.Builder adb = new AlertDialog.Builder(this);
+        AlertDialog ad = adb.create();
+        ad.setTitle("ALERT");
+        ad.setMessage("The grammar can not be parsed. Choose a different parser or grammar");
+        ad.show();
+        finish();
+    }
 
 
 }
